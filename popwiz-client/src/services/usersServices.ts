fda51ee5a1cb467131599e415b301d17ab4b15a8
LironDs <david.liron@gmail.com>
2024-01-03 23:48:50 +0200

    update

--- popwiz-client/src/services/usersServices.ts
+++ popwiz-client/src/services/usersServices.ts
@@ -3,35 +3,35 @@ import User from "../interfaces/User";
 import { jwtDecode } from "jwt-decode";
 import { TokenDetails } from "../App";
 
-let api: string = `${process.env.REACT_APP_API}/users`;
+let api: string = `${process.env.REACT_APP_API}`;
 type userLogin = Pick<User, "email" | "password">;
 
 ////Register
 export function addUser(newUser: User) {
-  return axios.post(`http://localhost:10000/api/register`, newUser);
+  return axios.post(`${api}/register`, newUser);
 }
 ///Login
 export function checkUser(userToCheck: userLogin) {
-  return axios.post(`http://localhost:10000/api/login`, userToCheck);
+  return axios.post(`${api}/login`, userToCheck);
 }
 
 ////Update user
 export function updateUser(_id: string, updatedUser: User) {
-  return axios.put(`${api}/${_id}`, updatedUser, {
+  return axios.put(`${api}/users/${_id}`, updatedUser, {
     headers: {
       Authorization: sessionStorage.getItem("token") as string,
     },
   });
 }
 export function getUserById(_id: string) {
-  return axios.get(`${api}/${_id}`, {
+  return axios.get(`${api}/users/${_id}`, {
     headers: {
       Authorization: sessionStorage.getItem("token") as string,
     },
   });
 }
 export function getUsers() {
-  return axios.get(api, {
+  return axios.get(`${api}/users`, {
     headers: {
       Authorization: sessionStorage.getItem("token") as string,
     },
@@ -39,7 +39,7 @@ export function getUsers() {
 }
 
 export function deleteUser(_id: string) {
-  return axios.delete(`${api}/${_id}`, {
+  return axios.delete(`${api}/users/${_id}`, {
     headers: {
       Authorization: sessionStorage.getItem("token") as string,
     },
